2abea1c8d284bd587593729997ce1782e62ee3b7
Nikolaj JK <kagenogmig@gmail.com>
2023-07-18 21:51:46 +0200

    add command line arguments
    it is added both in code and explanation of usage in README

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ A limitation of the project right know is that the settings must be changed in t
 # Usage
 To simply run the project use
 ```
-cargo run -r <source> <destination>
+cargo run -r -- [options] <source> <destination>
 ```
 If you want the binary then in the root directory of the repository use the following to build the project in release mode (`-r`)
 ```
@@ -16,5 +16,12 @@ You can find the binary at `target/release/thread-art-rust` if you are using Lin
 
 You use it with
 ```
-./thread-art-rust <source> <destination>
+./thread-art-rust [options] <source> <destination>
 ```
+
+The options are
+- `--size -s` (image size `400`),
+- `--wraps -w` (the number of thread wraps `2500`),
+- `--points -p` (points on circle `235`),
+- `--minimum_difference -m` (points inbetween next choice `20`),
+- `--brightness_factor -b` (the amount a pixel is brightnened by `51`).
--- src/main.rs
+++ src/main.rs
@@ -6,20 +6,6 @@ use std::io::BufWriter;
 use std::io::Write;
 use std::path::Path;
 
-// IMAGE_SIZE is the diameter of the frame in mm, if the thread width
-// is 1 mm. If it is .5 mm, then IMAGE_SIZE should be doubled.
-//     IMAGE_SIZE = frame_width(mm) / thread_width(mm).
-// E.g. a 40 cm frame with a .5 mm wide thread should have:
-//     IMAGE_SIZE = 400 / 0.5 = 800
-// which affects the result, so consider changing number of wraps, 
-// bWhichrightness factor etc.
-const IMAGE_SIZE: u16 = 400;
-const IMAGE_AREA: usize = IMAGE_SIZE as usize * IMAGE_SIZE as usize;
-const WRAPS: u16 = 2500 - 1;
-const CIRCLE_POINTS: u16 = 235;
-const MINIMUM_DIFFERENCE: u8 = 20;
-const BRIGHTNESS_FACTOR: u8 = 34;
-
 // represents a point/coordinate on the image.
 #[derive(Debug, PartialEq, Eq)]
 struct Point {
@@ -35,9 +21,48 @@ struct LineID {
 }
 
 fn main() {
-    // open the file into array.
+    // Default settings
+    // IMAGE_SIZE is the diameter of the frame in mm, if the thread width
+    // is 1 mm. If it is .5 mm, then IMAGE_SIZE should be doubled.
+    //     IMAGE_SIZE = frame_width(mm) / thread_width(mm).
+    // E.g. a 40 cm frame with a .5 mm wide thread should have:
+    //     IMAGE_SIZE = 400 / 0.5 = 800
+    // which affects the result, so consider changing number of wraps, 
+    // bWhichrightness factor etc.
+    let mut image_size = 400;
+    let mut wraps = 2500 - 1;
+    let mut circle_points = 235;
+    let mut minimum_difference = 20;
+    let mut brightness_factor = 51;
+    let mut verbose = false;
+    
+    // get command line arguments
     let args: Vec<String> = env::args().collect();
-    let input_path = Path::new(&args[1]);
+
+    // at minimum source and destination should be given (2 arguments + 1)
+    if args.len() < 3 {
+        panic!("Missing arguments.");
+    }
+    let mut iterator = args[1..args.len()-2].iter();
+    while let Some(opt) = iterator.next() {
+        match opt.as_str() {
+            "--size" | "-s" => image_size = get_argument_value(&mut iterator),
+            "--wraps" | "-w" => wraps = get_argument_value(&mut iterator),
+            "--points" | "-p" => circle_points = get_argument_value(&mut iterator),
+            "--minimum_difference" | "-m" => minimum_difference = get_argument_value(&mut iterator),
+            "--brightness_factor" | "-b" => brightness_factor = get_argument_value(&mut iterator),
+            "--verbose" | "-v" => verbose = true,
+            _ => panic!("Not a valid argument.")
+        }
+    }
+    if verbose {
+        print_settings(image_size, wraps, circle_points, minimum_difference, brightness_factor, &args);
+    }
+
+    let image_area = image_size * image_size;
+    
+    // open the file into array.
+    let input_path = Path::new(&args[args.len()-2]);
     let mut decoder = png::Decoder::new(File::open(input_path).unwrap());
     decoder.set_transformations(png::Transformations::normalize_to_color8());
     let mut reader = decoder.read_info().unwrap();
@@ -45,19 +70,19 @@ fn main() {
     let info = reader.next_frame(&mut buf).unwrap();
     let size = info.buffer_size();
     let bytes = &mut buf[..size];
-    if size != IMAGE_AREA {
+    if size != image_area {
         // the image is not the correct size
-        std::process::exit(1);
+        panic!("Problem with image: Probably not grayscale.");
     }
 
-    let circle_coords = circle_coords();
+    let circle_coords = circle_coords(image_size, circle_points);
     let lines = map_of_lines(&circle_coords);
 
     let mut point_index = 0;
     let mut point_list = vec![point_index];
     let mut used_lines = HashSet::new();
 
-    for _ in 0..WRAPS {
+    for _ in 0..wraps {
         let mut max_weight = 0;
         let mut max_line = &vec![Point { x: 0, y: 0 }];
         let mut max_point_index = 0;
@@ -68,8 +93,8 @@ fn main() {
             }
 
             let difference = (next_point_index as isize - point_index as isize).abs();
-            if difference < MINIMUM_DIFFERENCE as isize
-                || difference > (circle_coords.len() - MINIMUM_DIFFERENCE as usize) as isize
+            if difference < minimum_difference as isize
+                || difference > (circle_coords.len() - minimum_difference as usize) as isize
             {
                 continue;
             }
@@ -81,7 +106,7 @@ fn main() {
                 .unwrap();
             let mut weight = line.len() * 255;
             for pos in line {
-                let pixel = bytes[IMAGE_SIZE as usize * pos.y as usize + pos.x as usize];
+                let pixel = bytes[image_size as usize * pos.y as usize + pos.x as usize];
                 weight = std::cmp::max(weight as isize - pixel as isize, 0) as usize;
             }
 
@@ -99,22 +124,21 @@ fn main() {
         point_index = max_point_index;
 
         for pos in max_line {
-            let pixel_value = bytes[IMAGE_SIZE as usize * pos.y as usize + pos.x as usize];
-            let value = std::cmp::min(255, pixel_value as usize + BRIGHTNESS_FACTOR as usize) as u8;
-            bytes[IMAGE_SIZE as usize * pos.y as usize + pos.x as usize] = value;
+            let pixel_value = bytes[image_size as usize * pos.y as usize + pos.x as usize];
+            let value = std::cmp::min(255, pixel_value as usize + brightness_factor as usize) as u8;
+            bytes[image_size as usize * pos.y as usize + pos.x as usize] = value;
         }
     }
 
-    let mut output: [u8; IMAGE_AREA] =
-        [255; IMAGE_AREA];
+    let mut output: Vec<u8> = vec![255; image_area];
     for i in 1..point_list.len() {
         let p1 = point_list[i - 1] as u16;
         let p2 = point_list[i] as u16;
         let line = lines.get(&get_line_id(p1, p2)).unwrap();
         for pos in line {
-            let c_value = output[IMAGE_SIZE as usize * pos.y as usize + pos.x as usize];
+            let c_value = output[image_size as usize * pos.y as usize + pos.x as usize];
             let value = std::cmp::max(c_value as isize - 20 as isize, 0) as u8;
-            output[IMAGE_SIZE as usize * pos.y as usize + pos.x as usize] = value;
+            output[image_size as usize * pos.y as usize + pos.x as usize] = value;
         }
     }
 
@@ -123,7 +147,7 @@ fn main() {
     let points_file = File::create(points_path).unwrap();
     let mut points_writer = BufWriter::new(points_file);
     points_writer
-        .write((CIRCLE_POINTS.to_string() + "\n").as_bytes())
+        .write((circle_points.to_string() + "\n").as_bytes())
         .unwrap();
 
     for point in point_list {
@@ -132,15 +156,33 @@ fn main() {
     }
 
     // write the resulting image
-    let output_path = Path::new(&args[2]);
+    let output_path = Path::new(args.iter().nth_back(0).unwrap());
     let output_file = File::create(output_path).unwrap();
     let ref mut output_writer = BufWriter::new(output_file);
 
-    let mut encoder = png::Encoder::new(output_writer, IMAGE_SIZE as u32, IMAGE_SIZE as u32);
+    let mut encoder = png::Encoder::new(output_writer, image_size as u32, image_size as u32);
     encoder.set_color(png::ColorType::Grayscale);
     encoder.set_depth(png::BitDepth::Eight);
     let mut writer = encoder.write_header().unwrap();
     writer.write_image_data(&output).unwrap();
+}
+
+fn get_argument_value(iterator: &mut std::slice::Iter<'_, String>) -> usize {
+    let v: usize = match iterator.next() {
+        None => panic!("Value not given to argument."),
+        Some(x) => x.parse().expect("A number.")
+    };
+    v
+}
+
+fn print_settings(image_size: usize, wraps: usize, circle_points: usize, minimum_difference: usize, brightness_factor: usize, args: &Vec<String>) {
+    println!("Size is {}", image_size);
+    println!("Wraps is {}", wraps);
+    println!("Points {}", circle_points);
+    println!("Minimum difference is {}", minimum_difference);
+    println!("Brightness factor is {}", brightness_factor);
+    println!("Source is {}", args.iter().nth_back(1).unwrap());
+    println!("Destination is {}", args.iter().nth_back(0).unwrap());
 }
 
 fn bresenham(x0: u16, y0: u16, x1: u16, y1: u16) -> Vec<Point> {
@@ -185,20 +227,20 @@ fn bresenham(x0: u16, y0: u16, x1: u16, y1: u16) -> Vec<Point> {
     }
 }
 
-fn circle_coords() -> Vec<Point> {
+fn circle_coords(image_size: usize, circle_points: usize) -> Vec<Point> {
     let mut circle_coords = Vec::new();
 
-    for i in 0..CIRCLE_POINTS {
+    for i in 0..circle_points {
         let (x, y);
-        let angle = std::f64::consts::PI * 2.0 / CIRCLE_POINTS as f64 * i as f64;
-        let size_half = (IMAGE_SIZE / 2) as f64;
+        let angle = std::f64::consts::PI * 2.0 / circle_points as f64 * i as f64;
+        let size_half = (image_size / 2) as f64;
         x = constrain(
             (angle.cos() * size_half + size_half) as usize,
-            (IMAGE_SIZE - 1) as usize,
+            (image_size - 1) as usize,
         ) as u16;
         y = constrain(
             (angle.sin() * size_half + size_half) as usize,
-            (IMAGE_SIZE - 1) as usize,
+            (image_size - 1) as usize,
         ) as u16;
         circle_coords.push(Point { x: x, y: y });
     }
